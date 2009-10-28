f6c6e24b0cf0f7eb914d8ffb06b08175f1ff6a34
James Cleveland <jamescleveland@gmail.com>
2009-10-28 16:20:20 +0000

    gitignore, samples, etc

---
+++ config/database_sample.php
@@ -0,0 +1,9 @@
+<?php
+global $config_db;
+
+$config_db[ 'hostname' ] = 'hostname';
+$config_db[ 'username' ] = 'username';
+$config_db[ 'password' ] = 'password';
+$config_db[ 'database' ] = 'database';
+
+?>
---
+++ config/debug_sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Holds debug configuration details.
+ * 
+ * @package debug
+ * @subpackage configuration
+ */
+
+$config_debug[ 'fb_path' ] = 'fb_path';
+?>
