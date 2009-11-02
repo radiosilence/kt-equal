a5c9c18f0290f1433b44e7ea2b518b9be38e7ab6
unknown <Karl@.(none)>
2009-11-02 13:47:03 +0000

    hopefully removed non-sample config files??

--- config/auth.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config_auth[ "keyphrase" ] = "the quick brown fox was taking meth";
-$config_auth[ "base_salt" ] = "the lazy dog lives off benefits";
-?>
--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $config_db;
-
-$config_db[ 'hostname' ] = '127.0.0.1';
-$config_db[ 'username' ] = 'hci1';
-$config_db[ 'password' ] = 'PCZez2BZ5aZVHBwb';
-$config_db[ 'database' ] = 'hci1';
-
-?>
