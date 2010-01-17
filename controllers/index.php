14c9cb3e66774ee3009f95d68e066379815e84d7
James Cleveland <jamescleveland@gmail.com>
2010-01-17 22:44:31 +0000

    various

--- controllers/index.php
+++ controllers/index.php
@@ -3,8 +3,21 @@ class controller_index extends controller
 {
 	public function index( $args )
 	{
+		$navs = array( 
+			"home" 		=> "Home",
+			"about_us" 	=> "About Us",
+			"projects" 	=> "Projects",
+			"get_involved" 	=> "Get Involved",
+			"help" 		=> "Help",
+		);
+		
+		$subnavs = array( "temp" => "Temp", "hi" => "Hi" );
+
 		$tpl = new view( $this->registry );
 		$tpl->set( "title", "KT-EQUAL" );
+		$tpl->set( "navs", $navs );
+		$tpl->set( "subnavs", $subnavs );
+		$tpl->set( "page", "about_us" );
 		$tpl->set( "subview", "home_body" );
 		$tpl->show( "default" );
 	}
--- views/tmp.php
+++
@@ -1,4 +0,0 @@
-
-							<?php foreach( $navs as $n => $nav ): ?>
-							<li><a href="<?php echo $n?>" id="n_<?php echo $n?><?php echo ( $nav[ "active" ] ? "_active" : null )?>"><?php echo $nav[ "title" ]?></a></li>
-							<?php endforeach; ?>
