ef290477446997661762762267713ca9c490d76b
James Cleveland <jamescleveland@gmail.com>
2010-01-17 19:58:59 +0000

    added the default template in near completion stage, etc

--- controllers/index.php
+++ controllers/index.php
@@ -3,38 +3,10 @@ class controller_index extends controller
 {
 	public function index( $args )
 	{
-		$db 		= $this->database();
-		$articles 	= array();
-		$sth = $db->prepare( "
-			SELECT		id, title, body, date_added, author, publisher, date
-			FROM		articles
-			ORDER BY	date_added DESC
-			LIMIT		5
-		" );
-		
-		$sth->execute();
-		
-		while( $article = $sth->fetch() )
-		{
-			$seo_url = new seo_url( $article );
-			$s = new markdown_smartypantstypographer();
-			$articles[] = array(
-				"id"		=> $article[ "id" ],
-				"title" 	=> $article[ "title" ],
-				"excerpt" 	=> $s->transform (utf8_encode( substr( $article[ "body" ], 0, 200 ) )). "&hellip;",
-				"seo_url" 	=> $seo_url->url,
-				"date_added"	=> date( "dS M", strtotime( $article[ "date_added" ] )),
-				"author"	=> $article[ "author" ],
-				"publisher"	=> $article[ "publisher" ],
-				"date"		=> $article[ "date" ]
-			);
-		}
-		
 		$tpl = new view( $this->registry );
-		$tpl->set( "articles", $articles );
-		$tpl->set( "page_title", "Look At That F***ing Title" );
+		$tpl->set( "title", "KT-EQUAL" );
 		$tpl->set( "subview", "home_body" );
-		$tpl->show( "home" );
+		$tpl->show( "default" );
 	}
 }
 ?>
---
+++ views/default.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<base href="http://<?php echo HOST?>/<?php echo BASE_HREF ?>/">
+		<title><?php echo $title?> | KT-EQUAL</title>
+		<link rel="stylesheet" type="text/css" href="css_lib/960.css" />
+		<link rel="stylesheet" type="text/css" href="css/hci1.css" />
+		<script src="js_lib/jquery-1.4.css"></script>
+	</head>
+	<body>
+		<div id="header">
+			<div class="container_12">
+				<div class="grid_4" id="logo">
+					<a href="<?php echo BASE_HREF ?>"><img src="img/logo.png" alt="Logo" title="KT-EQUAL" /></a>
+				</div>
+				<div class="grid_8" id="tabs">
+					<div class="grid_3 prefix_5" id="search">
+						<input type="text" name="search" class="grid_3" value="Search"/>
+					</div>
+					<div id="primary_navigation">
+						Tabs
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="main">
+			<div class="container_12" id="infobox">
+				<div class="grid_4">
+					<img src="cnt/img/placeholder.jpg" />
+				</div>
+				<div class="grid_8">
+					<h1>Some shit about this section</h1>
+					<p>Maybe a paragraph about helping old people.</p>
+					<ul>
+						<li>Bullet points</li>
+						<li>Bullet points</li>
+						<li>Bullet points</li>
+					</ul>
+					<p>Look how amazingly large this text is and this is good because people can read it. Especially the clients of our site.</p>
+				</div>
+			</div>
+			<div class="container_12" id="content">
+				<div class="grid_12">
+					<h1>Oh Boy! A Heading!</h1>
+				</div>
+				<div class="grid_6">
+					<h2>Out with the old in with the new</h2>
+					<p>Funding for SPARC finished at the end of December 2008. We are now in transition to KT-EQUAL, a new project designed specifically to exploit a decade of investment by EPSRC in ageing and disability research. EPSRC’s EQUAL initiative showed that by keeping researchers close to research users and beneficiaries as well as ensuring that that they were networked to policy makers, major improvements, locally and nationally, could be achieved very quickly. EQUAL has had a significant influence on official regulations, standards, good practice and, importantly, expectations about the quality of the built environment, about the design of products and systems, and about the support which information technologies can provide to older and disabled people, in good and in poor health.</p>
+				</div>
+				<div class="grid_6">
+					<h2>Growing confidence</h2>
+					<p>Through the EQUAL Network and more recently SPARC (funded jointly by EPSRC and BBSRC), researchers throughout the UK have become familiar with the realities of growing older, frailty and disability. They know that these do not respect the boundaries of the scientific disciplines. Seeking out solutions, improving quality of life and wellbeing, requires multidisciplinary research teams, engagement with the many organisations and agencies which work with older and disabled people, and recognising the preferences, knowledge and expertise of older and disabled people and their carers.</p>
+
+					<p>Over the last seven years, EQUAL and SPARC have introduced many new scientists to the exciting challenges of ageing research, and have secured their long-term commitment to the area. They have helped to build a superb rapport across many different communities of stakeholders and interest groups, by showing that ageing and disability research can and does make a difference to the lives of people. This has resulted in an incredible level of support, interest and encouragement in this next stage of getting research into practice. </p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
